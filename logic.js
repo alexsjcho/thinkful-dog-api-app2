92ad3f3c2d753d52d7c38ad49c48dfce463733e1
Alex Cho <alexsjcho@gmail.com>
2019-02-06 14:29:01 -0800

    updated html, css, logic.js files

--- logic.js
+++ logic.js
@@ -1,18 +1,4 @@
 "use strict";
-//Pass numInput, which represents an integer as an argument
-function getDogImage(numInput) {
-  if (numInput < 3) {
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-      .then(response => response.json())
-      .then(responseJson => console.log(responseJson));
-  } else if (numInput > 50) {
-    return alert("Please choose a number equal or less than 50");
-  } else {
-    fetch(`https://dog.ceo/api/breeds/image/random/${numInput}`)
-      .then(response => response.json())
-      .then(responseJson => console.log(responseJson));
-  }
-}
 
 function watchUserInput() {
   $("#dog-num-form").submit(e => {
@@ -27,3 +13,30 @@ $(function() {
   console.log("App loaded! Waiting for submit!");
   watchUserInput();
 });
+
+//Pass numInput, which represents an integer as an argument
+function getDogImage(numInput) {
+  if (numInput < 3) {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then(response => response.json())
+      .then(responseJson =>  displayResults(responseJson))
+       .catch(error => alert('Something went wrong. Try again later.'));
+  } else if (numInput > 50) {
+    return alert("Please choose a number equal or less than 50");
+  } else {
+    fetch(`https://dog.ceo/api/breeds/image/random/${numInput}`)
+      .then(response => response.json())
+      .then(responseJson => displayResults(responseJson))
+       .catch(error => alert('Something went wrong. Try again later.'));
+  }
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.dog-img-results').replaceWith(
+    `<img src="${responseJson.message}" class="dog-img-results">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+}
